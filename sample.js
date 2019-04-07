7353efa786407dae3e8edb36ed772d01e8411a00
Jonatan Gall <jonatangd.souza@gmail.com>
2019-04-07 17:26:43 -0300

    grant permition

--- sample.js
+++ sample.js
@@ -1,65 +1,64 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * @author Jonatan Gall 
  */
 
 /* global getAssetRegistry getFactory emit */
 
 /**
  * Sample transaction processor function.
- * @param {org.example.basic.registerThirdPartServiceOnDevice} registerService The sample transaction instance.
+ * @param {org.example.basic.registerThirdPartServiceOnDevice} tx
  * @transaction
  */
-async function registerThirdPartServiceOnDevice(registerService) {  // eslint-disable-line no-unused-vars
+async function registerThirdPartServiceOnDevice(tx) {  // eslint-disable-line no-unused-vars
 
-    if(registerService.owner !=  registerService.device.owner){
+    if(tx.owner !=  tx.device.owner){
         throw new Error('Owner not registered on this device')
     }
     
-    const allowedUsers = registerService.device.allowedUsers;
-    if(allowedUsers.find((allowedUser) => allowedUser.thirdPartServiceId == registerService.thirdPartService.thirdPartServiceId)){
+    const allowedUsers = tx.device.allowedUsers;
+    if(allowedUsers.find((allowedUser) => allowedUser.thirdPartServiceId == tx.thirdPartService.thirdPartServiceId)){
         throw new Error('Third Party Service already been registered on this device')
     }
     try{
-        registerService.device.allowedUsers.push(registerService.thirdPartService);
+        tx.device.allowedUsers.push(tx.thirdPartService);
     }catch(e) {
         throw new Error('Cannot add users on this device ==> ', e);
     }
     let assetRegistry = await getAssetRegistry('org.example.basic.Device');
-    await assetRegistry.update(registerService.device);
+    await assetRegistry.update(tx.device);
 }
-/* global getAssetRegistry getFactory emit */
-
 /**
- * Sample transaction processor function.
- * @param {org.example.basic.removeThirdPartServiceOnDevice} registerService The sample transaction instance.
+ * Transaction remove a third part services from allowed services on device array participant
+ * @param {org.example.basic.removeThirdPartServiceOnDevice} tx
  * @transaction
  */
-async function removeThirdPartServiceOnDevice(registerService) {  // eslint-disable-line no-unused-vars
+async function removeThirdPartServiceOnDevice(tx) {  // eslint-disable-line no-unused-vars
 
-    if(registerService.owner !=  registerService.device.owner){
+    if(tx.owner !=  tx.device.owner){
         throw new Error('Owner not registered on this device')
     }
     
-    const allowedUsers = registerService.device.allowedUsers;
-    if(!allowedUsers.find((allowedUser) => allowedUser.thirdPartServiceId == registerService.thirdPartService.thirdPartServiceId)){
+    const allowedUsers = tx.device.allowedUsers;
+    if(!allowedUsers.find((allowedUser) => allowedUser.thirdPartServiceId == tx.thirdPartService.thirdPartServiceId)){
         throw new Error('Third Party Service is not registered on this device')
     }
     try{
-        registerService.device.allowedUsers = allowedUsers.filter((el) => el.thirdPartServiceId != registerService.thirdPartService.thirdPartServiceId);
+        tx.device.allowedUsers = allowedUsers.filter((el) => el.thirdPartServiceId != tx.thirdPartService.thirdPartServiceId);
     }catch(e) {
         throw new Error('Cannot add users on this device ==> ', e);
     }
 
     let assetRegistry = await getAssetRegistry('org.example.basic.Device');
-    await assetRegistry.update(registerService.device);
+    await assetRegistry.update(tx.device);
+}
+/**
+ * Transaction grant access to allowed Third Part Users
+ * @param {org.example.basic.grantThirdPartServicePermitionOnDevice} tx
+ * @transaction
+ */
+async function grantThirdPartServicePermitionOnDevice(tx) {  // eslint-disable-line no-unused-vars
+    const allowedUsers = tx.device.allowedUsers;
+    if(!allowedUsers.includes(tx.thirdPartService)){
+        throw new Error('Third Party Service is not allowed for this device');
+    }
 }
